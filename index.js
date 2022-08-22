812e32ec597f8345d459b8205a7dc874408624c8
Ana Carolina <102334478+AnaCarolinaMello@users.noreply.github.com>
2022-08-22 08:27:11 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,11 @@ const fs = require('fs')
 const port = 3000
 const route = require('./routes/route')
 const cors = require('cors')
+const swaggerUi = require('swagger-ui-express')
+const swaggerDocument = require('./swagger.js')
 
 app.use(express.json());
+app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 // app.use('/',route)
 
 app.get('/', async (req,res) =>{
